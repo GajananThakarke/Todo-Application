7cef6181e81783ed42d6ae33e27cf64e339907ef
GajananThakarke <110619113+GajananThakarke@users.noreply.github.com>
2024-07-09 00:10:58 +0530

    Delete Todo Application directory

--- Todo Application/todo-backend/src/config/database.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('todo_db', 'root', 'password', {
-  host: 'db',
-  dialect: 'mysql',
-});
-
-module.exports = sequelize;
